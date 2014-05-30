fd5379717e653d4d81e6206f2a650cab3ec4fdf2
Volodymyr Sergeyev <vova.sergeyev@gmail.com>
2014-05-30 12:10:49 +0300

    Obsolete code removal

--- ZenPacks/zenoss/Microsoft/MSMQ/impact.py
+++ ZenPacks/zenoss/Microsoft/MSMQ/impact.py
@@ -70,8 +70,10 @@ def getPoolTriggers(guid, format, **kwargs):
         )),
     )
 
+# ----------------------------------------------------------------------------
+# Impact relationships
 
-class BaseRelationsProvider(object):
+class MSMQQueueRelationsProvider(object):
     implements(IRelationshipDataProvider)
 
     relationship_provider = RP
@@ -89,54 +91,6 @@ class BaseRelationsProvider(object):
             self._guid = guid(self._object)
 
         return self._guid
-
-    def impact(self, relname):
-        relationship = getattr(self._object, relname, None)
-        if relationship:
-            if isinstance(relationship, ToOneRelationship):
-                obj = relationship()
-                if obj:
-                    yield edge(self.guid(), guid(obj))
-
-            elif isinstance(relationship, ToManyRelationshipBase):
-                for obj in relationship():
-                    yield edge(self.guid(), guid(obj))
-
-    def impacted_by(self, relname):
-        relationship = getattr(self._object, relname, None)
-        if relationship:
-            if isinstance(relationship, ToOneRelationship):
-                obj = relationship()
-                if obj:
-                    yield edge(guid(obj), self.guid())
-
-            elif isinstance(relationship, ToManyRelationshipBase):
-                for obj in relationship():
-                    yield edge(guid(obj), self.guid())
-
-    def getEdges(self):
-        if self.impact_relationships is not None:
-            for impact_relationship in self.impact_relationships:
-                for impact in self.impact(impact_relationship):
-                    yield impact
-
-        if self.impacted_by_relationships is not None:
-            for impacted_by_relationship in self.impacted_by_relationships:
-                for impacted_by in self.impacted_by(impacted_by_relationship):
-                    yield impacted_by
-
-
-class BaseTriggers(object):
-    implements(INodeTriggers)
-
-    def __init__(self, adapted):
-        self._object = adapted
-
-
-# ----------------------------------------------------------------------------
-# Impact relationships
-
-class MSMQQueueRelationsProvider(BaseRelationsProvider):
 
     def getEdges(self):
         yield edge(self.guid(), guid(self.device()))
